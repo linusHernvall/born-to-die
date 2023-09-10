5830c85d50336299d71119ffe4fe50ca9e748f66
Linus Hernvall <linus.hernvall@gmail.com>
2023-09-10 16:14:04 +0200

    How to style an input

--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -1,6 +1,5 @@
-import { useMutation } from "@apollo/client";
+import { Box, TextField } from "@mui/material";
 import gql from "graphql-tag";
-import { useState } from "react";
 
 const REGISTER_USER = gql`
   mutation register(
@@ -27,82 +26,39 @@ const REGISTER_USER = gql`
 `;
 
 function Register() {
-  const [values, setValues] = useState({
-    username: "",
-    email: "",
-    password: "",
-    confirmPassword: "",
-  });
-
-  const [registerUser, { loading, error }] = useMutation(REGISTER_USER, {
-    update(_, result) {
-      console.log(result);
-      // Redirect user or perform some other action
-    },
-    onError(err) {
-      console.error("Error registering user:", err);
-    },
-    variables: values,
-  });
-
-  const handleSubmit = (event: React.FormEvent) => {
-    event.preventDefault();
-    registerUser();
-  };
-
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setValues({ ...values, [event.target.name]: event.target.value });
-  };
-
   return (
-    <div>
-      <h2>Register</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Username:</label>
-          <input
-            type="text"
-            name="username"
-            placeholder="Username"
-            value={values.username}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <label>Email:</label>
-          <input
-            type="email"
-            name="email"
-            placeholder="Email"
-            value={values.email}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <label>Password:</label>
-          <input
-            type="password"
-            name="password"
-            placeholder="Password"
-            value={values.password}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <label>Confirm Password:</label>
-          <input
-            type="password"
-            name="confirmPassword"
-            placeholder="Confirm Password"
-            value={values.confirmPassword}
-            onChange={handleChange}
-          />
-        </div>
-        <button type="submit">Register</button>
-      </form>
-      {loading && <p>Loading...</p>}
-      {error && <p>Error: {error.message}</p>}
-    </div>
+    <Box>
+      <TextField
+        required
+        id="outlined-required"
+        label="Required"
+        defaultValue="Hello World"
+        variant="outlined"
+        sx={{
+          "& .MuiInputBase-input": {
+            color: "white",
+          },
+          "& .MuiFormLabel-root.Mui-focused": {
+            color: "white", // Change label color when focused
+          },
+          "& .MuiOutlinedInput-root": {
+            "& fieldset": {
+              borderColor: "white", // Change default border color
+            },
+
+            "&:hover fieldset": {
+              borderColor: "orange", // Change border color on hover
+            },
+            "&.Mui-focused fieldset": {
+              borderColor: "yellow", // Change border color when focused
+            },
+            "&.Mui-error fieldset": {
+              borderColor: "red", // Change border color when there's an error
+            },
+          },
+        }}
+      />
+    </Box>
   );
 }
 
